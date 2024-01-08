b3ade7085f0b1f8b0c936a5ba798337bba3193ea
Nadim Ahmad <nadimdayer7860@gmail.com>
2024-01-08 22:38:31 +0530

    completed

--- course-list/src/app/app.component.ts
+++ course-list/src/app/app.component.ts
@@ -10,5 +10,47 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'course-list';
+  title = 'Nadim Ahmad';
+  response ={
+    "customerInformatino": {
+        "firstname": "Liyakat",
+        "lastname": "Hussain",
+        "CoursesEnrolled": [
+            {
+                "id": 1,
+                "courseName": "Angular Course",
+                "duration": "30",
+                "ratings": "5",
+                "totalLectures": 5,
+                "completedLecture": 1,
+                "tutorName": "Test",
+                "price":400,
+                "topics": [
+                        "What is Angular",
+                        "Component in Angular",
+                        "Module in Angular",
+                        "Services in Angular",
+                        "Directive in Angular"
+                ]
+            },
+            {
+                "id": 2,
+                "courseName": "React Course",
+                "duration": "15",
+                "ratings": "4",
+                "totalLectures": 5,
+                "completedLecture": 1,
+                "tutorName": "Test 2",
+                "price":500,
+                "topics": [
+                    "What is React",
+                    "Component in React",
+                    "Module in React",
+                    "Services in React",
+                    "Directive in React"
+            ]
+            }
+        ]
+    }
+}
 }
